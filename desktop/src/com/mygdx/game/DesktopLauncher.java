b230f152d3597e1365ea39735d782496b8ebb83c
Guilherme <gggbsilvestre@usp.br>
2024-06-05 22:23:39 -0300

    Implementação de movimento e texturas

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,7 +2,8 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGame;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.mygdx.game.JavaInvaders;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +11,10 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("space-invaders");
-		new Lwjgl3Application(new MyGame(), config);
+		config.setWindowedMode(1900, 1080);
+		config.setResizable(false);
+		config.useVsync(true);
+
+		new Lwjgl3Application(new JavaInvaders(), config);
 	}
 }
